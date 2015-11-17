6b146cd84d3ead91072d601cd09b49b1a0486040
George Maizel <gmaizel@gmail.com>
2015-11-17 22:25:01 +0200

    Edit task description

--- js/TaskEditor.js
+++ js/TaskEditor.js
@@ -29,37 +29,58 @@ function TaskEditor()
 	this._layer.appendChild(this._container);
 
 	this._titleEditor = document.createElement("textarea");
-	this._titleEditor.addEventListener('input', this._onTextChange.bind(this));
+	this._titleEditor.className = "title";
+	this._titleEditor.addEventListener('input', this._onTitleChanged.bind(this));
 	this._titleEditor.addEventListener('contextmenu', function(event) { event.stopPropagation(); });
-	this._container.appendChild(this._titleEditor);
 
 	this._saveButton = document.createElement("input");
 	this._saveButton.type = "button";
 	this._saveButton.value = "Save";
 	this._saveButton.addEventListener('click', this._save.bind(this));
-	this._container.appendChild(this._saveButton);
 
 	this._cancelButton = document.createElement("input");
 	this._cancelButton.type = "button";
 	this._cancelButton.value = "Cancel";
 	this._cancelButton.addEventListener('click', this._cancel.bind(this));
+
+	this._descriptionEditor = document.createElement("textarea");
+	this._descriptionEditor.className = "description";
+	this._descriptionEditor.addEventListener('contextmenu', function(event) { event.stopPropagation(); });
+
+	this._titleEditor.tabIndex = 1;
+	this._descriptionEditor.tabIndex = 2;
+	this._saveButton.tabIndex = 3;
+	this._cancelButton.tabIndex = 4;
+
+	this._container.appendChild(this._titleEditor);
+	this._container.appendChild(this._saveButton);
 	this._container.appendChild(this._cancelButton);
+	this._container.appendChild(this._descriptionEditor);
 }
 
 TaskEditor.prototype.show = function(taskElement, taskDetails, callback)
 {
 	this._callback = callback;
+	this._taskElement = taskElement;
 	this._titleEditor.value = taskDetails.title || "";
+	this._descriptionEditor.value = taskDetails.description || "";
 
-	var rect = taskElement.getBoundingClientRect();
+	var rect = this._taskElement.getBoundingClientRect();
 	this._container.style.left = rect.left + "px";
 	this._container.style.top = rect.top + "px";
-	this._container.style.width = rect.width + "px";
+	this._titleEditor.style.width = rect.width + "px";
 	document.body.appendChild(this._layer);
+
+	// FIXME: scroll workspace to align task control with the editor if possible
+	// With current styles buttons may move when they get more room on the right,
+	// therefore alignment requires several iterations.
+	while (this._container.offsetLeft > 0 && this._container.offsetLeft + this._container.offsetWidth > this._layer.clientWidth) {
+		this._container.style.left = (this._layer.clientWidth - this._container.offsetWidth) + "px";
+	}
 
 	Keyboard.pushListener(this._onKeyDown.bind(this), null);
 	this._titleEditor.focus();
-	this._onTextChange();
+	this._onTitleChanged();
 }
 
 TaskEditor.prototype.hide = function()
@@ -76,7 +97,7 @@ TaskEditor.prototype._onKeyDown = function(event)
 		break;
 
 	case Keyboard.RETURN:
-		if (!event.ctrlKey && !event.shiftKey) {
+		if (event.ctrlKey) {
 			event.preventDefault();
 			this._save();
 		}
@@ -84,21 +105,27 @@ TaskEditor.prototype._onKeyDown = function(event)
 	}
 }
 
-TaskEditor.prototype._onTextChange = function()
+TaskEditor.prototype._onTitleChanged = function()
 {
+	var title = this._titleEditor.value.trim();
+	this._saveButton.disabled = !title;
+
 	this._titleEditor.style.height = "1px";
 	var h = this._titleEditor.scrollHeight;
 	this._titleEditor.style.height = h + "px";
 
-	var title = this._titleEditor.value.trim();
-	this._saveButton.disabled = !title;
+	// FIXME: scroll taskElement's parent view up to align it with title editor if possible.
+	var rect = this._taskElement.getBoundingClientRect();
+	var maxTop = this._layer.clientHeight - this._container.offsetHeight;
+	this._container.style.top = Math.max(0, Math.min(rect.top, maxTop)) + "px";
 }
 
 TaskEditor.prototype._save = function()
 {
 	var title = this._titleEditor.value.trim();
+	var description = this._descriptionEditor.value.trim();
 	if (title) {
-		this._callback({ title: title, description: "" });
+		this._callback({ title: title, description: description });
 	}
 }
 
